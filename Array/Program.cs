f50b244f1be5413e0a88ede34262bc272edcd440
dwpoiio <guillermo.larragodoy@gmail.com>
2023-05-08 11:34:23 -0300

    test4

--- Array/Program.cs
+++ Array/Program.cs
@@ -3,16 +3,11 @@
 namespace Array1
 {
     public class Program
-    {
+    {   
         public static void Main()
         {
             // arrayMethod();
-            var numbers = new List<int>() { 1,2,3,4};
-            numbers.Add(5);
-            numbers.AddRange(new int[3] { 6, 7, 8 });
-
-            foreach (var number in numbers)
-                Console.WriteLine(number);
+            listClass.listMethod();
         }
 
         private static void arrayMethod()
---
+++ Array/listClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Array1
+{
+	public class listClass
+	{
+		public static void listMethod()
+		{
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(5);
+            numbers.AddRange(new int[3] { 6, 7, 8 });
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+        }
+	}
+}
+
