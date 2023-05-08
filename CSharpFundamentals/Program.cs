dcbd487b38459c65c160ff9167f0803292540ffc
dwpoiio <guillermo.larragodoy@gmail.com>
2023-05-08 12:08:48 -0300

    CSharpfundamentals

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -8,7 +8,7 @@ namespace CSharpFundamentals
         static void Main(string[] args)
         {
             var john = new Person();
-            john.FirstName = "John";
+            john.FirstName = "Johnn";
             john.LastName = "Smith";
             //john.Introduce();
 
