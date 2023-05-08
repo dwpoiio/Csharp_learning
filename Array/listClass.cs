5c487ffc20a910f33d51b805d9c95f19f616c340
dwpoiio <guillermo.larragodoy@gmail.com>
2023-05-08 11:53:52 -0300

    List demo done

--- Array/listClass.cs
+++ Array/listClass.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace Array1
 {
 	public class listClass
@@ -6,11 +8,30 @@ namespace Array1
 		public static void listMethod()
 		{
             var numbers = new List<int>() { 1, 2, 3, 4 };
-            numbers.Add(5);
+            numbers.Add(1);
             numbers.AddRange(new int[3] { 6, 7, 8 });
 
             foreach (var number in numbers)
                 Console.WriteLine(number);
+
+            Console.WriteLine("Index of 1: " +numbers.IndexOf(1));
+            Console.WriteLine("Last index of 1: " + numbers.LastIndexOf(1));
+
+            Console.WriteLine("Count: " + numbers.Count);
+
+           
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
+            }
+
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            numbers.Clear();
+            Console.WriteLine("Count after clear(): " + numbers.Count);
         }
 	}
 }
