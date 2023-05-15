b2f373b62f13bfce03c2c66a2541a73e839bc49d
dwpoiio <guillermo.larragodoy@gmail.com>
2023-05-15 10:34:34 -0300

    Learning

--- CSharpFundamentals/TimeSpanclass.cs
+++ CSharpFundamentals/TimeSpanclass.cs
@@ -5,8 +5,34 @@ namespace CSharpFundamentals
 	{
 		public static void Method()
 		{
+			// Creating
+			var timespan = new TimeSpan(1, 2, 3);
 
-		}
+            var timespan1 = new TimeSpan(1, 0,0);
+			var timespan2 = TimeSpan.FromHours(1);
+
+			var start = DateTime.Now;
+            var endFixed = start.AddMinutes(2);
+            var end = DateTime.Now.AddMinutes(2);
+			var duration = endFixed - start;
+
+			Console.WriteLine("Duration: " + duration);
+
+			// Proeperties
+			Console.WriteLine("Minutes: " + timespan.Minutes);
+            Console.WriteLine("total Minutes: " + timespan.TotalMinutes);
+
+            // Add
+            Console.WriteLine("Comparative: " + timespan);
+            Console.WriteLine("Add Example: " + timespan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine("Subtract Example: " + timespan.Subtract(TimeSpan.FromMinutes(2)));
+
+			// ToString
+			Console.WriteLine("ToString: " + timespan.ToString());
+
+			// Parse
+			Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
+        }
 	}
-}
+} 
 
