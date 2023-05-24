34b6935b1057281923e84657f48ff76e9c709134
dwpoiio <guillermo.larragodoy@gmail.com>
2023-05-24 11:01:46 -0300

    SummaryText

--- String/Program.cs
+++ String/Program.cs
@@ -4,7 +4,10 @@
     {
         public static void Main()
         {
-            String_unit.Method();
+            //String_unit.Method();
+            var text = "This is going to be a really really really long";
+            var summary = SummaryText.Method(text, 22);
+            Console.WriteLine(summary);
         }
     }
 }
---
+++ String/SummaryText.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace String1
+{
+    public class SummaryText
+    {
+        public static string Method(string text, int maxLength = 20)
+        {
+            return SummerizeText(text, maxLength);
+        }
+        static string SummerizeText(string sentence, int maxLength)
+        {
+            if (sentence.Length < maxLength)
+            return sentence;
+           
+            var words = sentence.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                break;
+            }
+
+            return String.Join(" ", summaryWords) + "...";
+        
+        }
+    }
+}
+
