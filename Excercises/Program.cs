82574d3f35ae5333e819582e6331243fc3e4aa86
dwpoiio <guillermo.larragodoy@gmail.com>
2023-05-31 13:59:24 -0300

    A couple of excercises

---
+++ Excercises/Program.cs
@@ -0,0 +1,15 @@
+﻿using Excercise_43;
+using Excercise49;
+using Excercise56;
+
+namespace Excercises
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Ex43.Main();
+            Ex49.Main();
+        }
+    }
+}
